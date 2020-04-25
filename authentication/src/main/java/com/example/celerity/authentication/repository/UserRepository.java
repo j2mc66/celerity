82b8bda13246fd9687fbedfd6e13463c67bc688f
jjmunozc <jjcoronel@stefanini.com>
2020-04-25 18:16:53 -0500

    modificaciones de properties y configuracion de querydsl

--- authentication/src/main/java/com/example/celerity/authentication/repository/UserRepository.java
+++ authentication/src/main/java/com/example/celerity/authentication/repository/UserRepository.java
@@ -4,10 +4,11 @@ package com.example.celerity.authentication.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import com.example.celerity.authentication.domain.User;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User,Long>, QuerydslPredicateExecutor<User> {
 	
 	Optional<User> findByUsername(String name);
 }
