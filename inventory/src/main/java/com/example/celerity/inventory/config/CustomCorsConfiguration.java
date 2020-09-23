b666493cb9170c56c268c15652ed58b638494dd8
jjmunozc <jjcoronel@stefanini.com>
2020-09-23 12:11:31 -0500

    ajustes de cors

---
+++ inventory/src/main/java/com/example/celerity/inventory/config/CustomCorsConfiguration.java
@@ -0,0 +1,36 @@
+package com.example.celerity.inventory.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@Profile("local")
+public class CustomCorsConfiguration {
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.addAllowedHeader("*");
+		configuration.addAllowedMethod("*");
+		configuration.setAllowCredentials(Boolean.TRUE);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		
+		return source;
+
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		return new CorsFilter(corsConfigurationSource());
+	}
+}
--- inventory/src/main/java/com/example/celerity/inventory/integration/UserClient.java
+++ inventory/src/main/java/com/example/celerity/inventory/integration/UserClient.java
@@ -1,6 +1,7 @@
 package com.example.celerity.inventory.integration;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +9,8 @@ import com.example.celerity.dto.CustomPage;
 import com.example.celerity.dto.UserDto;
 
 
-@FeignClient(value = "authentication")
+@Component("authenticationClientService")
+@FeignClient(contextId = "authenticationClientService", value = "authentication")
 @RequestMapping("/user")
 public interface UserClient {
 	
